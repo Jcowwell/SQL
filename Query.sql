f2daa1fbfa22edcfbbad078d4672b0dc99142724
Jcowwell <Jcowwell@gmail.com>
2019-02-27 21:45:55 -0500

    SQL Queries Added

---
+++ Query.sql
@@ -0,0 +1,7 @@
+
+-- Query 1
+  Select * from MUDA.number where id > 5;
+-- Query 2
+  Select name from MUDA.people where LEFT(name, 1) LIKE "n";
+-- Query 3
+  Select * from MUDA.animal Where color LIKE "white";
